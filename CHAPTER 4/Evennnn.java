2c310c87f9aa352e73727d831f7eebcb330a49b2
Theophilus131 <theophilusumar27@gmail.com>
2025-04-23 23:08:44 +0100

    'today'

---
+++ CHAPTER 4/Evennnn.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+ public class Evennnn{
+ public static void main(String [] args){
+
+ Scanner input = new Scanner(System.in);
+  System.out.print("enter number: ");
+	int number=input.nextInt();   
+	int even= 0;
+	int counter=2;
+	
+	while(counter != 0 ){
+
+	if(number % 2== 0){
+   	}
+    	 counter += 2;
+	}
+
+System.out.print("the even numbers are :" + even);
+ 
+   }
+}
+
+
